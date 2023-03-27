19c7fc5bd2435efda61e028d751bd987782ff736
rewks <rewks@users.noreply.github.com>
2023-03-27 18:50:01 +0100

    Create basic grid structure of home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,22 @@ export default function Home() {
     return (
         <main>
             <div className={styles.main_container}>
-                t
+                <div className={styles.profile_picture}>
+                    pic
+                </div>
+                <div className={styles.blurb_text}>
+                    about me blah blah
+                </div>
+                <div className={styles.blog_card}>
+                    most recent post
+                </div>
+                <div className={styles.blog_card}>
+                    second most recent post
+                </div>
+                <div className={styles.blog_card}>
+                    third most recent post
+                </div>
             </div>
-      
-
         </main>
     )
 }
