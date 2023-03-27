b282ba41b9761b7197478d4f94c4cca12e0e4b47
rewks <rewks@users.noreply.github.com>
2023-03-27 19:28:06 +0100

    Update home page grid, insert avatar placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Home() {
     return (
         <main>
             <div className={styles.main_container}>
-                <div className={styles.profile_picture}>
-                    pic
+                <div className={styles.avatar_container}>
+                    <Image
+                        src="/../public/root/woman-asian-temples.png"
+                        alt="A.I generated art of the author"
+                        width={1280/5}
+                        height={1280/5}
+                        priority
+                        className={styles.avatar}
+                    />
                 </div>
-                <div className={styles.blurb_text}>
+                <div className={styles.blurb_container}>
                     about me blah blah
+                </div>
+                <div className={styles.console_container}>
+                    admin@rewks.com:~$ cd ~
                 </div>
                 <div className={styles.blog_card}>
                     most recent post
