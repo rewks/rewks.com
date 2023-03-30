4ed2bddb1ffe1bd18c844ad516d96a511bc0f201
rewks <rewks@users.noreply.github.com>
2023-03-30 23:52:54 +0100

    Remove unused code from postlist page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,4 @@
-import Link from 'next/link';
-
-export default function PostList({ posts }: { posts: string[] }) {
-  return (
-    <ul>
-      hi
-      {/* {posts.map((post) => (
-        <li key={post.id}>
-          <Link href={`/blog/${post.slug}`}>
-            {post.title}
-          </Link>
-        </li>
-      ))} */}
-    </ul>
-  );
+export default function PostList() {
+  return <>
+  </>
 }
