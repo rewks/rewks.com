95a8db541f9e0da8faed4b36459b0dec50d78ceb
rewks <rewks@users.noreply.github.com>
2023-04-07 14:36:42 +0100

    Create functions to compile metadata and content from mdx files

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,39 @@
 import Link from 'next/link';
+import fs from 'fs'
+import path from 'path'
+import { MDXRemote, compileMDX } from 'next-mdx-remote/rsc'
 
-export default function PostList() {
-  return (
-    <ul>
-      hi
-      {/* {posts.map((post) => (
-        <li key={post.id}>
-          <Link href={`/blog/${post.slug}`}>
-            {post.title}
-          </Link>
-        </li>
-      ))} */}
-    </ul>
-  );
+const getPostByPostPath = async (fileName: string) => {
+    const slug = fileName.replace('.mdx', '');
+    const postPath = path.join('posts', fileName);
+    const postContent = fs.readFileSync(postPath, { encoding: 'utf8' });
+
+    const { frontmatter, content } = await compileMDX({
+        source: postContent,
+        options: { parseFrontmatter: true }
+    })
+
+    return { meta: { ...frontmatter }, slug: slug, content }
+}
+
+const getAllPostsMeta = async () => {
+    const files = fs.readdirSync(path.join('posts'))
+    const promises = files.map(async (filename) => {
+        const { slug, meta } = await getPostByPostPath(filename)
+        return { slug, meta }
+    })
+    const posts = await Promise.all(promises)
+    return posts
+}
+
+export default async function PostList() {
+    const postList = await getAllPostsMeta();
+    return <>
+        {postList?.map(post => (
+            <Link href={`blog/${post.slug}`}>
+                <h1>{post?.slug}</h1>
+                <p>{post.meta.title as string}</p>
+            </Link>
+        ))}
+    </>
 }
