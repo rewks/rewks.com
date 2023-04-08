d4e1041a82b150d9619185519cce8605b6903478
rewks <rewks@users.noreply.github.com>
2023-04-08 22:24:58 +0100

    Improve referrer-policy header setting

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const Headers = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
     key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
+    value: 'strict-origin-when-cross-origin',
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
   {
