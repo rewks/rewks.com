8d60298fedc2983d54209d5d2b7d6e38056a79c1
rewks <rewks@users.noreply.github.com>
2023-04-10 00:22:55 +0100

    Create pagination functionality on blog listing page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,14 +2,29 @@ import { getAllPostsMeta } from '@/app/components/mdx-reader'
 import BlogCard from '@/app/components/blogCard'
 import styles from './page.module.css'
 
-export default async function PostList() {
+const postsPerPage = 3;
+
+type Props = {
+    searchParams?: {
+        page?: number,
+    },
+}
+
+export default async function PostList(props: Props) {
     const postList = await getAllPostsMeta();
     const sortedPostList = postList.sort((a, b) => {
         return (a?.meta.date as string) > (b?.meta.date as string) ? -1 : 1;
     })
+
+    let page = props.searchParams?.page? props.searchParams?.page : 1
+    const totalPages = Math.ceil(sortedPostList.length / postsPerPage)
+
+    page = page > totalPages ? 1 : page
+    const pagePostList = sortedPostList.slice(postsPerPage * (page - 1), postsPerPage * page)
+
     return <>
         <div className={styles.cards_container}>
-            {sortedPostList.map(post => (
+            {pagePostList.map(post => (
                 <BlogCard
                     key={post.slug}
                     slug={post.slug}
