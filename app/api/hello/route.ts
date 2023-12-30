04f217dbd087c58d5638b560d0e5764c8a3b55b7
rewks <46387812+rewks@users.noreply.github.com>
2023-12-30 20:10:00 +0000

    Create new nextjs 14 project

--- app/api/hello/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const id: string = searchParams.get('id') ?? 'friend';
-  return new Response(`Hello, ${id}!`, {
-    headers: {
-      'Server': 'FeatherFlow 1.12.3',
-    }
-  })
-}
